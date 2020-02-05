49c00b2d41eb6f99008e05fd5f0c2098f3c0a462
Thomas Munzer <thomas.munzer@gmail.com>
2020-02-05 21:33:45 +0100

    improve log messages

--- src/ex.py
+++ src/ex.py
@@ -39,40 +39,40 @@ def _ex_connect(dev, switch):
     try:
         dev.open()
         cu = Config(dev)
-        console.notice("connected to switch %s" %switch)
+        console.notice("SWITCH: %s | Connected" %switch)
         return cu
     except:
-        console.error("unable to connect to switch %s" %switch)
+        console.error("SWITCH: %s | Unable to connect" %switch)
         return None
 
 # lock ex configuration
 def _ex_lock(cu, switch):
     try:
         cu.lock()
-        console.notice("configuration locked on device %s" %switch)
+        console.notice("SWITCH: %s | Configuration locked" %switch)
         return True
     except:
-        console.error("unable to lock configuration on device %s" %switch)
+        console.error("SWITCH: %s | Unable to lock configuration" %switch)
         return False
 
 # commit ex configuration
 def _ex_commit(cu, switch):
     try:
         cu.commit()
-        console.notice("configuration commited on device %s" %switch)
+        console.notice("SWITCH: %s | Configuration commited" %switch)
         return True
     except:
-        console.error("unable to commit configuration on device %s" %switch)
+        console.error("SWITCH: %s | Unable to commit configuration" %switch)
         return False
 
 # unlock ex configuration
 def _ex_unlock(cu, switch):
     try:
         cu.unlock()
-        console.notice("configuration unlocked on device %s" %switch)
+        console.notice("SWITCH: %s | Configuration unlocked" %switch)
         return True
     except:
-        console.error("unable to unlock configuration on device %s" %switch)
+        console.error("SWITCH: %s | Unable to unlock configuration" %switch)
         return False
 
 # function called to change the ex configuration
@@ -94,13 +94,13 @@ def _change_ex_conf(switch, conf):
 
 def ap_connected(mac, lldp_system_name, lldp_port_desc):
     lldp_system_name = "%s.%s" %(lldp_system_name, domain_name)
-    console.info("AP %s connected on switch %s on port %s" %(mac, lldp_system_name, lldp_port_desc))
+    console.info("SWITCH: %s | PORT: %s | AP %s connected" %(lldp_system_name, lldp_port_desc, mac))
     conf = _replace_port(ex_conf_trunk_ap, lldp_port_desc)
     _change_ex_conf(lldp_system_name, conf)
 
 def ap_disconnected(mac, lldp_system_name, lldp_port_desc):
     lldp_system_name = "%s.%s" %(lldp_system_name, domain_name)
-    console.info("AP %s disconnected from switch %s on port %s" %(mac, lldp_system_name, lldp_port_desc))
+    console.info("SWITCH: %s | PORT: %s | AP %s disconnected" %(mac, lldp_system_name, lldp_port_desc, mac))
     conf = _replace_port(ex_conf_default, lldp_port_desc)
     _change_ex_conf(lldp_system_name, conf)
     
