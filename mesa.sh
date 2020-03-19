8982424c59af9021ef9aa3b8f0c0d3c408eca438
Thomas Munzer <thomas.munzer@gmail.com>
2020-03-19 13:01:49 +0100

    fix script

--- mesa.sh
+++ mesa.sh
@@ -762,9 +762,9 @@ function deploy
 function update_app
 {
   $DOCKER_COMP --file $PERSISTANT_FOLDER/$APP_NAME/docker-compose.yaml stop  
-  $DOCKER_COMP --file $PERSISTANT_FOLDER/$APP_NAME/docker-compose.yaml rm -f
+  $DOCKER_COMP --file $PERSISTANT_FOLDER/$APP_NAME/docker-compose.yaml rm --force
   $DOCKER_COMP --file $PERSISTANT_FOLDER/$APP_NAME/docker-compose.yaml pull
-  $DOCKER_COMP --file $PERSISTANT_FOLDER/$APP_NAME/docker-compose.yaml create
+  $DOCKER_COMP --file $PERSISTANT_FOLDER/$APP_NAME/docker-compose.yaml build --no-start
   compose_files="$(get_active_compose_files)"
   if [ -n "$compose_files" ]
   then
