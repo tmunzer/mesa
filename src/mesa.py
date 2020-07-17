e9815653fdcb5b391a55a9bcc75dbc1fea97aca2
Thomas Munzer <thomas.munzer@gmail.com>
2020-07-17 15:39:21 +0200

    fix external org_id

--- src/mesa.py
+++ src/mesa.py
@@ -8,7 +8,13 @@ from mesa_ctrl import Mesa
 from mesa_ctrl_ext import MesaExt
 from config import mist_conf
 server_uri = mist_conf["server_uri"]
+from config import external_conf
 
+if external_conf["enabled"]:
+    external_server_uri = external_conf["server_uri"]
+    external_org_id = external_conf["org_id"]
+else:
+    external_server_uri = "external"
 
 from mongo import MesaDB
 mongo_host = "mist-mongo"
@@ -44,25 +50,28 @@ def postJsonHandler():
                 Mesa(event, thread_id, mesa_db).start()
     return '', 200
 
-@app.route("/external", methods=["POST"])
+@app.route(external_server_uri, methods=["POST"])
 def postClearpassHandler():
-    global active_threads
-    global mesa_db
-    content = request.get_json()
-    event={}
-    event["mac"] = content["switch_mac"] if "switch_mac" in content else None
-    event["port"] = content["switch_port"] if "switch_port" in content else None
-    event["source"] = "clearpass"
-    if "type" in event :
-        event["type"] = content["type"]
-        if event["type"] == "AP_CONNECTED" or event["type"] == "AP_DISCONNECTED" or event["type"] == "AP_RESTARTED":
-            thread_id = active_threads
-            if active_threads == 1000:
-                active_threads = 1
-            else:
-                active_threads += 1
-            MesaExt(event, thread_id, mesa_db).start()
-    return '', 200
+    if external_conf["enabled"]:
+        global active_threads
+        global mesa_db
+        content = request.get_json()
+        event={}
+        event["mac"] = content["switch_mac"] if "switch_mac" in content else None
+        event["port"] = content["switch_port"] if "switch_port" in content else None
+        event["source"] = "clearpass"
+        event["org_id"] = external_org_id
+        if "type" in content :
+            event["type"] = content["type"]
+            if event["type"] == "AP_CONNECTED" or event["type"] == "AP_DISCONNECTED" or event["type"] == "AP_RESTARTED":
+                thread_id = active_threads
+                if active_threads == 1000:
+                    active_threads = 1
+                else:
+                    active_threads += 1
+                MesaExt(event, thread_id, mesa_db).start()
+        return '', 200
+    else: return '',404
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=server_port)
